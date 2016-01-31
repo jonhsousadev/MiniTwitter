1558503406a9fde018fd5fe9762289cf58427f27
JonhSousa <jonhnbsousa@gmail.com>
2016-01-30 21:21:11 -0300

    Adding facebook authentication strategy

--- minitwitter/config/initializers/devise.rb
+++ minitwitter/config/initializers/devise.rb
@@ -145,7 +145,7 @@ Devise.setup do |config|
 
   # ==> Configuration for :validatable
   # Range for password length.
-  config.password_length = 8..72
+  config.password_length = 8..128
 
   # Email regex used to validate email formats. It simply asserts that
   # one (and only one) @ exists in the given string. This is mainly
@@ -262,4 +262,9 @@ Devise.setup do |config|
   # When using OmniAuth, Devise cannot automatically set OmniAuth path,
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
+
+    #Add your ID and secret here
+  #ID first, secret second
+  config.omniauth :facebook, "817535375039367", "6622499d53bd8415b904173738ebb55f", scope: 'email', info_fields: 'email'
+
 end
