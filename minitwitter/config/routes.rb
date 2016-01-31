7a44a39a21ddb70eaa098b879f7295cb86e622ce
JonhSousa <jonhnbsousa@gmail.com>
2016-01-31 20:24:56 -0300

    Adding home page

--- minitwitter/config/routes.rb
+++ minitwitter/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'home/index'
+
   get 'users/index'
 
   get 'users/:id/follow', to: 'users#follow', as: :follow
@@ -15,7 +17,7 @@ Rails.application.routes.draw do
         post :show
     end
   end
-    root 'articles#index'
+    root 'home#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
